40622a238095c0444f10c7f15bae6c4299f5d783
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-03-02 12:16:25 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 
 const puppeteer = require('puppeteer');
-var minutemail = require('10minutemail')
 
 init();
 
 async function init() {
-	await sendApiKeyToEmail2();
+	await sendApiKeyToEmail();
 	console.log('Finish');
 }
 
-async function sendApiKeyToEmail2(email) {
+async function sendApiKeyToEmail() {
 	// const args = ['--proxy-server=169.57.157.148:80'];
 	const browser = await puppeteer.launch();
+	const context = await browser.createIncognitoBrowserContext();
 
-	const page = await browser.newPage();
-	await page.goto("https://10minutemail.net/", { waitUntil: 'networkidle2' });
-	const element = await page.$("#fe_text");
-	email = await page.evaluate(element => element.value, element);
+	const emailPage = await context.newPage();
+	await emailPage.goto("https://10minutemail.net/", { waitUntil: 'networkidle2' });
+	let emailSelector = await emailPage.$("#fe_text");
+	let email = await emailPage.evaluate(emailSelector => emailSelector.value, emailSelector);
 	console.log(email)
 
-	let res = await page.evaluate(() => {
-		return document.getElementById("maillist").firstElementChild.childNodes.length - 1;
-	});
+	let emailResult = await emailPage.evaluate(() => document.getElementById("maillist").firstElementChild.childNodes.length - 1);
 
-	console.log(res, email);
+	console.log(emailResult, email);
 
+	try {
+		const tinyPage = await context.newPage();
+		await tinyPage.goto("https://tinypng.com/developers", { waitUntil: 'networkidle2' });
+		await tinyPage.focus("form.developers input[name='fullName']");
+		await tinyPage.keyboard.type('Max Molod');
+		await tinyPage.focus("form.developers input[name='mail']");
+		await tinyPage.keyboard.type("rph66912@zzrgg.com");
+		await tinyPage.click("form.developers input[type='submit']");
 
-	const page2 = await browser.newPage();
-	await page2.goto("https://tinypng.com/developers", { waitUntil: 'networkidle2' });
-	await page2.focus("form.developers input[name='fullName']");
-	await page2.keyboard.type('Max Molod');
-	await page2.focus("form.developers input[name='mail']");
-	await page2.keyboard.type(email);
-	await page2.click("form.developers input[type='submit']");
-	console.log('Email send!')
+		let isError = await tinyPage.evaluate(() => document.querySelector('.error').textContent);
+		if (isError) {
+			throw isError.textContent
+		} else {
+			console.log('Email send!');
+		}
+	}
+	catch (err) {
+		reject(err)
+	}
 
-
-	const element2 = await page.$("#fe_text");
-	email = await page.evaluate(element2 => element2.value, element2);
-	console.log(email);
-	console.log("Wait 1 minutes...");
+	console.log("Wait email from tiny...");
 	await new Promise((resolve, reject) => {
 		let count = 0;
 		let interval = setInterval(async () => {
-			let res2 = await page.evaluate(() => document.getElementById("maillist").firstElementChild.childNodes.length - 1);
+			let res2 = await emailPage.evaluate(() => document.getElementById("maillist").firstElementChild.childNodes.length - 1);
 			count++;
-			if (res < res2) {
+			if (emailResult < res2) {
 				clearInterval(interval);
 				resolve();
 			}
@@ -53,21 +57,22 @@ async function sendApiKeyToEmail2(email) {
 				clearInterval(interval);
 				reject("Email not delivered!");
 			}
-			console.log(res, res2);
+			console.log(emailResult, res2);
 		}, 15000)
 	});
-	let link = await page.evaluate(() => document.querySelector("#maillist tr:nth-child(2) td:nth-child(2) a").href);
-	await page.goto(link, { waitUntil: 'networkidle2' });
-	const element4 = await page.$("#fe_text");
-	email = await page.evaluate(element4 => element4.value, element4);
-	console.log(link, email);
 
-	let link2 = await page.evaluate(() => document.querySelector("div.mailinhtml .button a").href);
-	await page.goto(link2, { waitUntil: 'networkidle2' });
+	let getLinkToMail = await emailPage.evaluate(() => document.querySelector("#maillist tr:nth-child(2) td:nth-child(2) a").href);
+	await emailPage.goto(getLinkToMail, { waitUntil: 'networkidle2' });
 
-	let KEY = await page.evaluate(() => document.querySelector("tbody .key span").textContent);
+	console.log(getLinkToMail);
 
-	console.log(KEY);
+	let getLinkToTiny = await emailPage.evaluate(() => document.querySelector("div.mailinhtml .button a").href);
+	await emailPage.goto(getLinkToTiny, { waitUntil: 'networkidle2' });
 
-	await browser.close();
+	let getApiKey = await emailPage.evaluate(() => document.querySelector("tbody .key span").textContent);
+
+	console.log(getApiKey);
+
+	await context.close();
+	resolve();
 }
